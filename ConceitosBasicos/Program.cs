9f82b0af6a5ef268dc8ae18ecc5bd957cadf8d91
Aula <Aula@CANP-LABTI7-C01>
2024-03-07 17:48:23 -0300

    Adicionar arquivos de projeto.

---
+++ ConceitosBasicos/Program.cs
@@ -0,0 +1,17 @@
+﻿// crie um programa para calcular o IMC de uma pessoa e informar o status de acordo com o imc calculado
+
+Console.WriteLine("Informe seu peso:");
+int peso = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Informe sua altura:");
+float altura = float.Parse(Console.ReadLine());
+
+float imc = peso / (altura * altura);
+
+Console.WriteLine($"Para o peso {peso} e a altura {altura} o imc calculado foi {imc:f1}");
+
+string status = string.Empty;
+if (imc <= 16.9)
+    status = "muito abaixo do peso";
+else if (imc <= 18.4)
+    status = "abaixo do peso";
