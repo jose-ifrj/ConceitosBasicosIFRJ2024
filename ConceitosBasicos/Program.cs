8d923dd619172c5b1e4662f0d90cf971080bbf36
José <jose.ifrj.pinheiral>
2024-03-14 17:44:51 -0300

    aula 14/03

--- ConceitosBasicos/Program.cs
+++ ConceitosBasicos/Program.cs
@@ -1,17 +1,44 @@
 ﻿// crie um programa para calcular o IMC de uma pessoa e informar o status de acordo com o imc calculado
 
-Console.WriteLine("Informe seu peso:");
-int peso = int.Parse(Console.ReadLine());
+int opcao = 0;
 
-Console.WriteLine("Informe sua altura:");
-float altura = float.Parse(Console.ReadLine());
+do
+{
+    
+    Console.WriteLine("Informe o que deseja fazer:");
+    Console.WriteLine("(1) Calcular IMC");
+    Console.WriteLine("(0) Encerrar");
+    opcao = Convert.ToInt32(Console.ReadLine());
+    if (opcao == 1)
+        CalcularIMC();
 
-float imc = peso / (altura * altura);
+} while (opcao != 0);
+Console.WriteLine("Obrigado por sua participação!");
+Console.WriteLine("Programa Finalizado.");
 
-Console.WriteLine($"Para o peso {peso} e a altura {altura} o imc calculado foi {imc:f1}");
+static void CalcularIMC()
+{
+    Console.WriteLine("Informe seu peso:");
+    int peso = int.Parse(Console.ReadLine());
 
-string status = string.Empty;
-if (imc <= 16.9)
-    status = "muito abaixo do peso";
-else if (imc <= 18.4)
-    status = "abaixo do peso";
+    Console.WriteLine("Informe sua altura:");
+    float altura = float.Parse(Console.ReadLine());
+
+    float imc = peso / (altura * altura);
+
+    Console.WriteLine($"Para o peso {peso} e a altura {altura} o imc calculado foi {imc:f1}");
+
+    string status = string.Empty;
+    if (imc <= 16.9)
+        status = "muito abaixo do peso";
+    else if (imc <= 18.4)
+        status = "magreza";
+    else if (imc <= 29.9)
+        status = "Sobrepeso";
+    else if (imc <= 39.9)
+        status = "Obesidade";
+    else
+        status = "Obesidade Grave";
+
+    Console.WriteLine($"O IMC {imc} indica a classificação {status}");
+}
