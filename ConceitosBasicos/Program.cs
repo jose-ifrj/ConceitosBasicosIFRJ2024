9d67d627f9aea85ea139d1e29eae399a91524273
rosenclever <rosenclever@gmail.com>
2024-03-15 11:47:16 -0300

    peso ideal e jogo adivinhacao ok

--- ConceitosBasicos/Program.cs
+++ ConceitosBasicos/Program.cs
@@ -2,18 +2,38 @@
 
 // modifique o programa para que possua um menu de opções para suas funcionalidades
 
+using System.Globalization;
+
 int opcao = 0;
 
 do
 {
     Console.WriteLine("Informe o que deseja fazer:");
     Console.WriteLine("(1) Calcular IMC");
-    Console.WriteLine("(2) Jogo de adivinhação");
+    Console.WriteLine("(2) Peso Ideal");
+    Console.WriteLine("(3) Jogo de adivinhação");
     Console.WriteLine("(0) Encerrar");
     opcao = Convert.ToInt32(Console.ReadLine());
+    switch (opcao)
+    {
+        case 1:
+            CalcularIMC();
+            break;
+        case 2:
+            PesoIdeal();
+            break;
+        case 3:
+            JogoAdivinhacao();
+            break;
+    }
+    /*
     if (opcao == 1)
         CalcularIMC();
-
+    else if (opcao == 2)
+        PesoIdeal();
+    else if (opcao == 3)
+        JogoAdivinhacao();
+    */
 } while (opcao != 0);
 Console.WriteLine("Obrigado por sua participação!");
 Console.WriteLine("Programa Finalizado");
@@ -24,7 +44,7 @@ static void CalcularIMC()
     int peso = int.Parse(Console.ReadLine());
 
     Console.WriteLine("Informe sua altura:");
-    float altura = float.Parse(Console.ReadLine());
+    float altura = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
     float imc = peso / (altura * altura);
 
@@ -47,4 +67,45 @@ static void CalcularIMC()
     // crie a mensagem: O IMC x indica a classificação y
     // $ -> caractere de interpolação
     Console.WriteLine($"O IMC {imc} indica a classificação {status}");
+}
+
+static void PesoIdeal()
+{
+    Console.WriteLine("Informe sua altura:");
+    float altura = float.Parse (Console.ReadLine(), CultureInfo.InvariantCulture);
+    float minIdeal = (float)(Math.Pow(altura, 2) * 18.5);
+    float maxIdeal = (float)(Math.Pow(altura, 2) * 24.9);
+    Console.WriteLine($"Pelo IMC o peso ideal de uma pessoa com altura {altura}");
+    Console.WriteLine($" fica entre {minIdeal:f1} e {maxIdeal:f1}");
+}
+
+static void JogoAdivinhacao()
+{
+    Console.WriteLine("você tem 10 tentativas para adivinhar um número entre 1 e 100!");
+    Random random = new Random();
+    int valorSorteado = random.Next(1,100);
+    int tentativa;
+    for(int i = 0; i < 10; i++)
+    {
+        Console.WriteLine($"{i+1}a. tentativa:");
+        tentativa = Convert.ToInt32(Console.ReadLine());
+        if(tentativa == valorSorteado)
+        {
+            Console.WriteLine($"Parabéns!! Você adivinhou o número em {i} tentativas!!!");
+            break;
+        }
+        if(i < 9)
+        {
+            if (tentativa < valorSorteado)
+                Console.WriteLine("O valor sorteado é maior que o número informado. Tente novamente!");
+            else if (tentativa > valorSorteado)
+                Console.WriteLine("O valor sorteado é menor que o número informado. Tente novamente!");
+        }
+        else
+        {
+            Console.WriteLine("Você não possui mais tentativas!");
+        }
+            
+    }
+    Console.WriteLine("Fim de Jogo!");
 }
